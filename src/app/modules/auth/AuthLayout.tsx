553cb2293f0567bdc8b67cdb45fbb897a1401111
James S Schuler <66280149+refocusTheory@users.noreply.github.com>
2022-12-06 20:25:14 -0500

    Initial setup

--- src/app/modules/auth/AuthLayout.tsx
+++ src/app/modules/auth/AuthLayout.tsx
@@ -60,9 +60,7 @@ const AuthLayout = () => {
         {/* begin::Content */}
         <div className='d-flex flex-column flex-center py-15 px-5 px-md-15 w-100'>
           {/* begin::Logo */}
-          <Link to='/' className='mb-12'>
-            <img alt='Logo' src={toAbsoluteUrl('/media/logos/custom-1.png')} className='h-75px' />
-          </Link>
+
           {/* end::Logo */}
 
           {/* begin::Image */}
